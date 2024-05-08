5273613cd3fd3edf7c5d77f2f3a1fa9e02f34edf
Abhisek Panda <abhisekpanda789@gmail.com>
2024-05-09 00:57:09 +0530

    style:completed home page

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,12 +1,80 @@
+import { useState } from "react";
+import { FaPlus, FaMinus } from "react-icons/fa";
+
+import Union from "../assets/union.png";
+
+const faqTitles = [
+  {
+    title: "Do you offer freelancers?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, ipsum?",
+  },
+  {
+    title:
+      "Whats the guarantee that I will be satisfied with the hired talent?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, ipsum?",
+  },
+  {
+    title: "Can I hire multiple talents at once?",
+    answer:
+      "If unhappy with a project, communicate with the freelancer, allow for revisions, and refer to the agreement. Escalate to platform support if needed, considering mediation. Review policies, seek collaborative solutions for resolution.",
+  },
+  {
+    title: "Why should I not go to an agency directly?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, ipsum?",
+  },
+  {
+    title: "Who can help me pick a right skillset and duration for me?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, ipsum?",
+  },
+];
+
 const Faq = () => {
+  const [id, setId] = useState();
   return (
-    <section>
-      <div>
-        <p> What's on Your mind</p>
-        <p>Ask Questions</p>
-        <img src="" alt="" />
+    <section className="]">
+      <div className="bg-[#E8EEE7] mx-4 flex items-end rounded-[40px] h-[749px]">
+        <div className="relative">
+          <div className="flex flex-col justify-center items-start gap-5 absolute w-[405px] h-[111px] top-[-180px] left-[94.5px]">
+            <p className="w-[215px] h-[25px] not-italic font-normal text-[32px] leading-[110%] flex items-center tracking-[-0.02em] text-[#9E9D9D] flex-none order-none grow-0 font-covered">
+              {" "}
+              Whats on Your mind
+            </p>
+            <p className="w-[405px] h-[66px] not-italic font-semibold text-6xl leading-[110%] flex items-center tracking-[-0.01em] text-[#1C1C1C] flex-none order-1 grow-0 font-manrope">
+              Ask Questions
+            </p>
+          </div>
+          <img src={Union} alt="Union Logo" />
+        </div>
+        <div className="flex flex-col items-start gap-9 relative w-[581px]  p-0 left-48 my-auto font-manrope">
+          {faqTitles.map((que, i) => {
+            return (
+              <div key={i} className="w-[581px] flex flex-col justify-between">
+                <div className="flex flex-row justify-between items-center border border-solid border-b-[#D7D7D7] pb-4 ">
+                  <h3 className="w-[411px] h-14 not-italic font-semibold text-xl leading-[140%] flex items-center text-[#1C1C1C] flex-none order-none grow-0  my-0 font-manrope">
+                    {que.title}
+                  </h3>
+                  <button onClick={() => setId(i === id ? undefined : i)}>
+                    {i === id ? (
+                      <FaMinus className="h-[14px] w-[14px]" />
+                    ) : (
+                      <FaPlus className="h-[14px] w-[14px]" />
+                    )}
+                  </button>
+                </div>
+                {i === id && (
+                  <p className=" not-italic relative font-normal text-[15px] leading-[160%] text-[#617275] flex-none order-1 self-stretch grow-0 font-manrope">
+                    {que.answer}
+                  </p>
+                )}
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div></div>
     </section>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
+import { FaRegCopyright } from "react-icons/fa6";
+
 const Footer = () => {
   return (
-    <footer className="">
-      <div className="flex justify-between items-center ">
-        <p>Talup 2023. All rights reserved</p>
-        <div className="flex">
-          <p>Terms & Conditions</p>
-          <p>Privacy Policy</p>
+    <footer>
+      <div className="flex justify-between items-center gap-2.5 flex-none order-1 grow-0 p-[60px] rounded-[40px] bg-[#f5f5f5] mx-4 mb-6">
+        <p className=" not-italic font-medium text-lg leading-[110%] flex items-center text-[#1C1C1C] flex-none  grow-0 font-manrope justify-center">
+          <span>
+            <FaRegCopyright className="h-[20px] w-[20px] text-[#1c1c1c] mr-2" />
+          </span>
+          Talup 2023. All rights reserved
+        </p>
+        <div className="flex gap-[52px]">
+          <p className="font-manrope w-[162px] h-5 not-italic font-normal text-lg leading-[110%] flex items-center underline text-[#1C1C1C] flex-none order-none grow-0">
+            Terms & Conditions
+          </p>
+          <p className="font-manrope w-[162px] h-5 not-italic font-normal text-lg leading-[110%] flex items-center underline text-[#1C1C1C] flex-none order-none grow-0">
+            Privacy Policy
+          </p>
         </div>
       </div>
     </footer>
